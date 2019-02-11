0c335e3d581247bcecaa7f7ee31d2a0877f7bd45
Artur <artur.prado@gmail.com>
2019-02-11 09:18:02 -0200

    Add name to user and improve home page description

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_02_11_000834) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
