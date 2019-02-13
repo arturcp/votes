a02870cf018099e18bce90e5058a59229a602512
Artur <artur.prado@gmail.com>
2019-02-13 14:33:50 -0200

    Remove cache

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -10,19 +10,17 @@ class Ranking
   end
 
   def self.calculate(poll)
-    Rails.cache.fetch("ranking-for-#{poll.slug}", expires_in: 1.minute) do
-      list = {}
-      poll.categories.each do |category|
-        votes = Vote.where(category_id: category.id)
-        votes.each do |vote|
-          next if vote.candidate_id != category.candidate_id
+    list = {}
+    poll.categories.each do |category|
+      votes = Vote.where(category_id: category.id)
+      votes.each do |vote|
+        next if vote.candidate_id != category.candidate_id
 
-          list[vote.user_id] ||= RankingUser.new(user_name: vote.user.name)
-          list[vote.user_id].add_points(category.points)
-        end
+        list[vote.user_id] ||= RankingUser.new(user_name: vote.user.name)
+        list[vote.user_id].add_points(category.points)
       end
-
-      list.values.sort
     end
+
+    list.values.sort
   end
 end
